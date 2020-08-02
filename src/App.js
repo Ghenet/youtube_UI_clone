b92f56f53a08b4b52bea9c458f8f8c0c9b7913e4
Ghenet Andemiachael <ghenetandemiachael@Ghenets-MacBook-Pro.local>
2020-08-02 10:07:02 -0700

    adds the header component to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from './Header';
 import './App.css';
 
 function App() {
   return (
     //BEM class naming convention
     <div className="app">
+      <Header />
       <p>
         {/* Header -> <Header/> */}
         {/*Sidebar -> <Sidebar /> */}
