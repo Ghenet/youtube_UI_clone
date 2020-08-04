3275f3fdd3bfb4652f9253d45ec10945c8173f10
Ghenet Andemiachael <ghenetandemiachael@Ghenets-MacBook-Pro.local>
2020-08-04 13:46:54 -0700

    divides the header in to 3 containers

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Header.css';
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 import VideoCallIcon from '@material-ui/icons/VideoCall';
@@ -9,15 +10,22 @@ import Avatar from '@material-ui/core/Avatar';
 function Header() {
     return (
         <div className="header">
-            <MenuIcon />
-            <img className="header__logo" src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg" alt="" />
-            <input type="text" />
-            <SearchIcon />
-            <VideoCallIcon />
-            <AppsIcon />
-            <NotificationsIcon />
-            <Avatar alt="Gheni" src="" />
-        </div>
+            <div className="header__left">
+                <MenuIcon />
+                <img className="header__logo" src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg" alt="" />
+            </div>
+
+            <div className="header__input">
+                <input type="text" />
+                <SearchIcon />
+            </div>
+            <div className="header__icons">
+                <VideoCallIcon />
+                <AppsIcon />
+                <NotificationsIcon />
+                <Avatar alt="Gheni" src="" />
+            </div>
+        </div >
     )
 }
 
