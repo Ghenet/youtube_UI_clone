1670d33b6f0f017e6c14b1dd995ef08ec40ab993
Ghenet Andemiachael <ghenetandemiachael@Ghenets-MacBook-Pro.local>
2020-08-04 13:05:20 -0700

    adds the search and VideoCall Icon

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import VideoCallIcon from '@material-ui/icons/VideoCall';
 
 function Header() {
     return (
         <div className="header">
             <MenuIcon />
             <img className="header__logo" src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg" alt="" />
+            <input type="text" />
+            <SearchIcon />
+            <VideoCallIcon />
         </div>
     )
 }
