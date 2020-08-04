604d2f8916913351dd543583884769c6befd02f6
Ghenet Andemiachael <ghenetandemiachael@Ghenets-MacBook-Pro.local>
2020-08-04 13:00:23 -0700

    adds the youTube logo img

--- src/Header.js
+++ src/Header.js
@@ -5,6 +5,7 @@ function Header() {
     return (
         <div className="header">
             <MenuIcon />
+            <img className="header__logo" src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg" alt="" />
         </div>
     )
 }
