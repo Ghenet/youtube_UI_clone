3ac4c8c9a1e8144a371fa09374493ad3fbb67d75
Ghenet Andemiachael <ghenetandemiachael@Ghenets-MacBook-Pro.local>
2020-08-13 08:53:48 -0700

    flexs the recommendedVideos to take 80% of the container

---
+++ src/RecommendedVideos.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './RecommendedVideos.css';
+
+function RecommendedVideos() {
+    return (
+        <div className="recommendedVideos">
+            i'm reccomended videos.
+        </div>
+    )
+}
+
+export default RecommendedVideos
