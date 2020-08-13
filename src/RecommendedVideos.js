600709b52171f7e2d47f8258282cc2cf2da4b7dd
Ghenet Andemiachael <ghenetandemiachael@Ghenets-MacBook-Pro.local>
2020-08-13 13:39:41 -0700

    displays the recommendedVideos in flex

--- src/RecommendedVideos.js
+++ src/RecommendedVideos.js
@@ -1,12 +1,72 @@
-import React from 'react';
-import './RecommendedVideos.css';
+import React from "react";
+import "./RecommendedVideos.css";
+import VideoCard from "./VideoCard";
 
 function RecommendedVideos() {
-    return (
-        <div className="recommendedVideos">
-            <h2>Recommended</h2>
-        </div>
-    )
+  return (
+    <div className="recommendedVideos">
+      <h2>Recommended</h2>
+      <div className="recommendedVideos__videos">
+        <VideoCard
+          title="It's just me myself & I"
+          views="100M views"
+          timestamp="2 hrs ago"
+          channelImage=""
+          channel="Ghenet Kibrom"
+          image=""
+        />
+        {/* <VideoCard /> */}
+        <VideoCard
+          title="It's just me myself & I"
+          views="100M views"
+          timestamp="2 hrs ago"
+          channelImage=""
+          channel="Ghenet Kibrom"
+          image=""
+        />
+        <VideoCard
+          title="It's just me myself & I"
+          views="100M views"
+          timestamp="2 hrs ago"
+          channelImage=""
+          channel="Ghenet Kibrom"
+          image=""
+        />
+        <VideoCard
+          title="It's just me myself & I"
+          views="100M views"
+          timestamp="2 hrs ago"
+          channelImage=""
+          channel="Ghenet Kibrom"
+          image=""
+        />
+        <VideoCard
+          title="It's just me myself & I"
+          views="100M views"
+          timestamp="2 hrs ago"
+          channelImage=""
+          channel="Ghenet Kibrom"
+          image=""
+        />
+        <VideoCard
+          title="It's just me myself & I"
+          views="100M views"
+          timestamp="2 hrs ago"
+          channelImage=""
+          channel="Ghenet Kibrom"
+          image=""
+        />
+        <VideoCard
+          title="It's just me myself & I"
+          views="100M views"
+          timestamp="2 hrs ago"
+          channelImage=""
+          channel="Ghenet Kibrom"
+          image=""
+        />
+      </div>
+    </div>
+  );
 }
 
-export default RecommendedVideos
+export default RecommendedVideos;
