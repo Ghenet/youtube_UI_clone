c217bf7c7e9eab0ae4ec19473315b726ab4ba438
Ghenet Andemiachael <ghenetandemiachael@Ghenets-MacBook-Pro.local>
2020-08-13 09:24:10 -0700

    adds the icons from material ui and pass the Icon component as props from SidebarRow component

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './Sidebar.css';
 import SidebarRow from './SidebarRow';
+import HomeIcon from '@material-ui/icons/Home';
+import WhatshotIcon from '@material-ui/icons/Whatshot';
+import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
 
 function Sidebar() {
     return (
         <div className="sidebar">
-            i'm sidebar.
-            <SidebarRow title="Home" />
-            <SidebarRow title="Trending" />
-            <SidebarRow title="Subscription" />
+
+            <SidebarRow Icon={HomeIcon} title="Home" />
+            <SidebarRow Icon={WhatshotIcon} title="Trending" />
+            <SidebarRow Icon={SubscriptionsIcon} title="Subscription" />
         </div>
     )
 }
