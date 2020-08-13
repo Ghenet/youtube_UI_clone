fb7059e06f1950bb1734f6d8193873a163556bdc
Ghenet Andemiachael <ghenetandemiachael@Ghenets-MacBook-Pro.local>
2020-08-13 12:53:23 -0700

    adds the videoCard component and passes the elements in the card as props

---
+++ src/VideoCard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function VideoCard({ image, title, channel, views, timestamp }) {
+  return (
+    <div>
+      <h3>Video</h3>
+    </div>
+  );
+}
+
+export default VideoCard;
