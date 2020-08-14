59f35c4389d85c4e8f2834853741a4af60781df7
Ghenet Andemiachael <ghenetandemiachael@Ghenets-MacBook-Pro.local>
2020-08-13 20:07:07 -0700

    adds the home and search routers using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
-import React from 'react';
-import Header from './Header';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Header from "./Header";
 import Sidebar from "./Sidebar";
 import RecommendedVideos from "./RecommendedVideos";
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     //BEM class naming convention
     <div className="app">
-      <Header />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/search">
+            <h1>Search PAge</h1>
+          </Route>
+          <Route path="/">
+            <div className="app__page">
+              <Sidebar />
+              <RecommendedVideos />
+            </div>
+          </Route>
+        </Switch>
+      </Router>
+      {/* <Header />
       <div className="app__page">
         <Sidebar />
         <RecommendedVideos />
-
-      </div>
+      </div> */}
     </div>
   );
 }
