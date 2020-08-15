3f5342b1f958a9185fda02f71114d2a46715c4ef
Ghenet Andemiachael <ghenetandemiachael@Ghenets-MacBook-Pro.local>
2020-08-15 14:02:42 -0700

    adds the props from search page

--- src/ChannelRow.js
+++ src/ChannelRow.js
@@ -1,7 +1,10 @@
 import React from "react";
 
-function ChannelRow() {
-  return <div></div>;
+function ChannelRow( image, channel, verified
+    subs, noOfVideos, description) {
+  return <div className="channelRow">
+      
+  </div>;
 }
 
 export default ChannelRow;
