c90f6c696a9eb810ab8ccf1b3ebcdf51929cf5e2
Ghenet Andemiachael <ghenetandemiachael@Ghenets-MacBook-Pro.local>
2020-08-15 14:19:31 -0700

    typo correction of channel

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -13,7 +13,7 @@ function SearchPage() {
       <hr />
       <ChannelRow
         image="https://yt3.ggpht.com/a/AATXAJw84jaeCGDeXXau38GqfsCcy5mo9aq1ru3c1rPnag=s100-c-k-c0xffffffff-no-rj-mo"
-        Channel="Meghan Trainor"
+        channel="Meghan Trainor"
         verified
         subs="11.2M"
         noOfVideos={400}
