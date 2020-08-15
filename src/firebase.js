5661324cbc37d008da3cc1a3131b0277a872589d
Ghenet Andemiachael <ghenetandemiachael@Ghenets-MacBook-Pro.local>
2020-08-15 13:04:10 -0700

    styles the hr line below the filter icon

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCXiwbLNF-WF57Q0Vhh3JWhWPlr6JnHP0Y",
+    authDomain: "clone-950ed.firebaseapp.com",
+    databaseURL: "https://clone-950ed.firebaseio.com",
+    projectId: "clone-950ed",
+    storageBucket: "clone-950ed.appspot.com",
+    messagingSenderId: "799721360712",
+    appId: "1:799721360712:web:fc40cf940c74068395377d",
+    measurementId: "G-TDV8JN47B0"
+};
