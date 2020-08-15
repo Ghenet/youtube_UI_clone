a4d45b2928ff1efeaa8ae6fec90682b6240f88d0
Ghenet Andemiachael <ghenetandemiachael@Ghenets-MacBook-Pro.local>
2020-08-15 13:54:27 -0700

    adds the ChannerRow component

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -10,6 +10,14 @@ function SearchPage() {
         <h2>FILTER</h2>
       </div>
       <hr />
+      <ChannelRow
+        image="https://yt3.ggpht.com/a/AATXAJw84jaeCGDeXXau38GqfsCcy5mo9aq1ru3c1rPnag=s100-c-k-c0xffffffff-no-rj-mo"
+        Channel="Meghan Trainor"
+        verified
+        subs="11.2M"
+        noOfVideos={400}
+        description="Underated Best singer"
+      />
     </div>
   );
 }
